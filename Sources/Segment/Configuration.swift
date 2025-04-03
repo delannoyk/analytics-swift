1e4402d7fd8b64365e84281879455114387ea21b
Kevin Delannoy <kevin.delannoy@spothero.com>
2025-04-03 16:03:12 -0500

    feat: Allow for more granular tracked lifecycle events to track

--- Sources/Segment/Configuration.swift
+++ Sources/Segment/Configuration.swift
@@ -47,11 +47,53 @@ public enum StorageMode {
 
 // MARK: - Internal Configuration
 
+@objc(SEGTrackedLifecycleEvent)
+public final class TrackedLifecycleEvent: NSObject, OptionSet {
+    public let rawValue: Int
+
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+
+    @objc public static let none = TrackedLifecycleEvent()
+    @objc public static let applicationInstalled = TrackedLifecycleEvent(rawValue: 0b00000001)
+    @objc public static let applicationUpdated = TrackedLifecycleEvent(rawValue: 0b00000010)
+    @objc public static let applicationOpened = TrackedLifecycleEvent(rawValue: 0b00000100)
+    @objc public static let applicationBackgrounded = TrackedLifecycleEvent(rawValue: 0b00001000)
+    @objc public static let applicationForegrounded = TrackedLifecycleEvent(rawValue: 0b00010000)
+    #if os(macOS)
+    @objc  public static let applicationUnhidden = TrackedLifecycleEvent(rawValue: 0b00100000)
+    @objc  public static let applicationHidden = TrackedLifecycleEvent(rawValue: 0b01000000)
+    @objc public static let applicationTerminated = TrackedLifecycleEvent(rawValue: 0b10000000)
+
+    @objc public static let all = TrackedLifecycleEvent([
+        applicationInstalled,
+        applicationUpdated,
+        applicationOpened,
+        applicationBackgrounded,
+        applicationForegrounded,
+        applicationUnhidden,
+        applicationHidden,
+        applicationTerminated,
+    ])
+    #elseif os(iOS) || os(tvOS) || os(visionOS) || targetEnvironment(macCatalyst)
+    @objc public static let all = TrackedLifecycleEvent([
+        applicationInstalled,
+        applicationUpdated,
+        applicationOpened,
+        applicationBackgrounded,
+        applicationForegrounded,
+    ])
+    #else
+    @objc public static let all = TrackedLifecycleEvent.none
+    #endif
+}
+
 public class Configuration {
     internal struct Values {
         var writeKey: String
         var application: Any? = nil
-        var trackApplicationLifecycleEvents: Bool = true
+        var trackedApplicationLifecycleEvents = TrackedLifecycleEvent.all
         var flushAt: Int = 20
         var flushInterval: TimeInterval = 30
         var defaultSettings: Settings? = nil
@@ -110,8 +152,19 @@ public extension Configuration {
     /// - Parameter enabled: A bool value
     /// - Returns: The current Configuration.
     @discardableResult
+    @available(*, deprecated, message: "Use `setTrackedApplicationLifecycleEvents(_:)` for more granular control")
     func trackApplicationLifecycleEvents(_ enabled: Bool) -> Configuration {
-        values.trackApplicationLifecycleEvents = enabled
+        values.trackedApplicationLifecycleEvents = enabled ? .all : .none
+        return self
+    }
+
+    /// Opt-in/out of tracking lifecycle events.  The default value is `.none`.
+    ///
+    /// - Parameter events: An option set of the events to track.
+    /// - Returns: The current Configuration.
+    @discardableResult
+    func setTrackedApplicationLifecycleEvents(_ events: TrackedLifecycleEvent) -> Configuration {
+        values.trackedApplicationLifecycleEvents = events
         return self
     }
 
